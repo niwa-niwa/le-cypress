896d8c9806042bd534cd0f938b7f16f9d4cb842f
niwa-niwa <urban.hang274@gmail.com>
2022-12-29 23:25:58 +0900

    step2 text inputs

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,5 +1,19 @@
-describe('Input form', ()=>{
-  it('focuses input on load', ()=>{
-    cy.visit("http://localhost:3030")
-  })
-})
+describe("Input form", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("focuses input on load", () => {
+    // cy.visit("http://localhost:3030");
+
+    cy.focused().should("have.class", "new-todo");
+  });
+
+  it("accepts input", () => {
+    const item = "Buy Milk";
+
+    // cy.visit("http://localhost:3030");
+
+    cy.get(".new-todo").type(item).should("have.value", item);
+  });
+});
