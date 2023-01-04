4928bf70d4a8f6592b705634635dfff76d18b88b
niwa-niwa <urban.hang274@gmail.com>
2023-01-04 14:02:12 +0900

    Step6 toggling and debugging

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 import Footer from "./Footer";
-import { saveTodo, loadTodos, destroyTodo } from "../lib/service";
+import { saveTodo, loadTodos, destroyTodo, updateTodo } from "../lib/service";
 
 export default class TodoApp extends Component {
   constructor(props) {
@@ -19,6 +19,8 @@ export default class TodoApp extends Component {
     this.handleTodoSubmit = this.handleTodoSubmit.bind(this);
 
     this.handleDelete = this.handleDelete.bind(this);
+
+    this.handleToggle = this.handleToggle.bind(this);
   }
 
   componentDidMount() {
@@ -35,6 +37,32 @@ export default class TodoApp extends Component {
     destroyTodo(id).then(() =>
       this.setState({ todos: this.state.todos.filter((t) => t.id !== id) })
     );
+  }
+
+  handleToggle(id) {
+    const targetTodo = this.state.todos.find((t) => t.id === id);
+
+    const updated = {
+      ...targetTodo,
+      isComplete: !targetTodo.isComplete,
+    };
+
+    updateTodo(updated).then(({ data }) => {
+      // debug statement for cypress
+      // debugger
+
+      // const targetIndex = this.state.todos.findIndex((t) => t.id === data.id);
+
+      // const todos = [
+      //   ...this.state.todos.slice(0, targetIndex),
+      //   data,
+      //   ...this.state.todos.slice(targetIndex + 1),
+      // ];
+
+      const todos = this.state.todos.map((t) => (t.id === data.id ? data : t));
+    
+      this.setState({ todos: todos });
+    });
   }
 
   handleTodoSubmit(evt) {
@@ -71,6 +99,7 @@ export default class TodoApp extends Component {
             <TodoList
               todos={this.state.todos}
               handleDelete={this.handleDelete}
+              handleToggle={this.handleToggle}
             />
           </section>
           <Footer remaining={remaining} />
